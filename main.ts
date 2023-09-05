4f7c26c9cf6afb58c5618c3efb23fb38e25d0b27
ajonespenn <141892794+ajonespenn@users.noreply.github.com>
2023-09-04 20:41:20 -0700

    Changed to use Pin 2

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showIcon(IconNames.Sad)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . #
+    `)
 basic.forever(function () {
     while (input.buttonIsPressed(Button.A)) {
-        pins.digitalWritePin(DigitalPin.P16, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
     }
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
